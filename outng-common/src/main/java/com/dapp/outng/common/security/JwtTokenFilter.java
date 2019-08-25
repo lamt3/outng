2b0aebcde2964d42b088c99f05386e01a95afbe8
Tomoki Lam <tomoki_lam@apple.com>
2019-08-25 02:31:28 -0700

    fiixing security logic

--- outng-common/src/main/java/com/dapp/outng/common/security/JwtTokenFilter.java
+++ outng-common/src/main/java/com/dapp/outng/common/security/JwtTokenFilter.java
@@ -8,22 +8,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.dapp.outng.common.exception.OutngRuntimeException;
 import com.dapp.outng.common.http.SecurityConstants;
 
-@Component
+//@Component
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-    @Autowired
+	private static final Logger LOG = LoggerFactory.getLogger(JwtTokenFilter.class);
+	
+
     private JwtTokenProvider jwtTokenProvider;
+    
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+    	this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -31,6 +37,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         if (StringUtils.isNotEmpty(jwtToken) && jwtTokenProvider.validateToken(jwtToken)) {
             String userId = jwtTokenProvider.getUserIdFromJwt(jwtToken);
+            LOG.info("Identifying userId: {}", userId);
 
             if (StringUtils.isNotEmpty(userId)) {
                 Authentication authentication = new UsernamePasswordAuthenticationToken(userId, null, null);
--- web-outng-orchestrator/src/main/java/com/dapp/web/outng/orchestrator/configs/SecurityConfig.java
+++ web-outng-orchestrator/src/main/java/com/dapp/web/outng/orchestrator/configs/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.dapp.web.outng.orchestrator.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.dapp.outng.common.http.UrlConstants;
+import com.dapp.outng.common.security.JwtTokenFilter;
+import com.dapp.outng.common.security.JwtTokenProvider;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig {
 
 	@Configuration
@@ -16,18 +23,21 @@ public class SecurityConfig {
 	public class JwtSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 		static final int ORDER = 1;
 
-//		@Autowired
-//		private JwtTokenFilter jwtTokenFilter;
+		@Autowired 
+		private JwtTokenProvider provider;
 
-		private String[] sPath = UrlConstants.SERVICES_CONTEXT_PATH;
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/api/v1/auth/**");
+		}
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 //            http.headers().xssProtection().and().httpStrictTransportSecurity().and().contentTypeOptions();
-//            http.requestMatchers().antMatchers(sPath);
-//            http.authorizeRequests().anyRequest().authenticated();
-//            http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-			http.authorizeRequests().antMatchers("/").permitAll();
+
+			http.antMatcher("/api/v1/services/**").authorizeRequests() //
+					.anyRequest().authenticated() //
+					.and().addFilterBefore(new JwtTokenFilter(provider), UsernamePasswordAuthenticationFilter.class);
 			http.csrf().disable();
 		}
 	}
